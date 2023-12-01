dac74d70067801012b2514e19f4ab86f305ad87f
Andreas Storesund Madsen <andreas@asmadsen.no>
2023-12-01 06:57:28 +0100

    Solution for day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,46 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val validSpelledOutDigits = listOf(
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ) + (0..9).map { "$it" }
+
+    fun String.firstDigit(): Int {
+        val (_, word) = findAnyOf(validSpelledOutDigits) ?: error("Could not find any digit, spelled out or not")
+
+        return if (word.length == 1) {
+            word.toInt()
+        } else {
+            validSpelledOutDigits.indexOf(word) + 1
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun String.lastDigit(): Int {
+        val (_, word) = findLastAnyOf(validSpelledOutDigits) ?: error("Could not find any digit, spelled out or not")
+
+        return if (word.length == 1) {
+            word.toInt()
+        } else {
+            validSpelledOutDigits.indexOf(word) + 1
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part1(input: List<String>): Int = input.map { line ->
+        val firstDigit = line.first { it.isDigit() }
+        val lastDigit = line.last { it.isDigit() }
+        "$firstDigit$lastDigit".toInt()
+    }.sum()
+
+    fun part2(input: List<String>): Int = input.map { line ->
+        val firstDigit = line.firstDigit()
+        val lastDigit = line.lastDigit()
+
+        "$firstDigit$lastDigit".toInt()
+    }.sum()
+    readInput("Day01_test").let { testInput ->
+        check(part1(testInput) == 142)
+    }
+    readInput("Day01_test2").let { testInput ->
+        check(part2(testInput) == 281)
+    }
 
     val input = readInput("Day01")
     part1(input).println()
